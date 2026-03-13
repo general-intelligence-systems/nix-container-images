78aa0a763b0deb026972d515521adc6e6aaaf0ca
Nathan Kidd <nathankidd@hey.com>
2026-03-13 23:17:48 +0000

    Quick Git Push

--- images/nix-for-github-actions.nix
+++ images/nix-for-github-actions.nix
@@ -22,12 +22,19 @@
     ruby
     glibc
     stdenv.cc.cc.lib
+    nix
   ];
+
+  extraCommands = ''
+    mkdir -p usr/bin
+    ln -s ../../bin/env usr/bin/env
+  '';
 
   config = {
     Env = [
       "LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib"
       "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      "NIX_CONFIG=experimental-features = nix-command flakes"
     ];
   };
 }
