0e029058d81ee1238998824b303009cee824ab45
Nathan Kidd <nathankidd@hey.com>
2026-03-14 01:35:00 +0000

    Quick Git Push

--- runners/nix-for-github-actions.nix
+++ runners/nix-for-github-actions.nix
@@ -3,10 +3,11 @@ let
   dockerfile = pkgs.writeText "Dockerfile" ''
     FROM ghcr.io/actions/actions-runner:latest
     USER root
-    RUN curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --init none --no-confirm
-    ENV PATH="/nix/var/nix/profiles/default/bin:''${PATH}"
-    RUN nix-daemon & sleep 1 && nix profile install nixpkgs#ruby nixpkgs#xxd nixpkgs#crane
+    RUN mkdir -p /nix && chown runner:runner /nix
     USER runner
+    RUN curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --no-daemon
+    ENV PATH="/home/runner/.nix-profile/bin:''${PATH}"
+    RUN . /home/runner/.nix-profile/etc/profile.d/nix.sh && nix profile install nixpkgs#ruby nixpkgs#xxd nixpkgs#crane
   '';
 in
 pkgs.stdenv.mkDerivation {
