bee14192882fa221529022a5fdcbac646c4a1394
Nathan Kidd <nathankidd@hey.com>
2026-03-14 01:48:56 +0000

    Quick Git Push

--- runners/nix-for-github-actions.nix
+++ runners/nix-for-github-actions.nix
@@ -5,10 +5,10 @@ let
     USER root
     RUN apt-get update && apt-get install -y --no-install-recommends xz-utils && rm -rf /var/lib/apt/lists/*
     RUN mkdir -p /nix && chown runner:runner /nix
+    RUN mkdir -p /etc/nix && echo "experimental-features = nix-command flakes" > /etc/nix/nix.conf
     USER runner
     RUN curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --no-daemon
     ENV PATH="/home/runner/.nix-profile/bin:''${PATH}"
-    RUN mkdir -p /home/runner/.config/nix && echo "experimental-features = nix-command flakes" > /home/runner/.config/nix/nix.conf
     RUN nix profile install nixpkgs#ruby nixpkgs#xxd nixpkgs#crane
     USER root
     RUN ln -sf /home/runner/.nix-profile/bin/* /usr/local/bin/
